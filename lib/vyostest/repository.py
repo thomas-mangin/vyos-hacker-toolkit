64e3765acf36f69f7164dec7054e9d1395b52f9f
Thomas Mangin <thomas.mangin@exa.net.uk>
2020-04-12 16:33:05 +0100

    do not fail on existing link

--- lib/vyostest/repository.py
+++ lib/vyostest/repository.py
@@ -102,9 +102,9 @@ def setup(conf):
 	check(*run(conf.ssh('router', f'sudo apt-get --yes upgrade')))
 	check(*run(conf.ssh('router', f'sudo apt-get --yes install {packages}')))
 
-	check(*run(conf.ssh('router', f'ln -s /usr/lib/python3/dist-packages/vyos vyos')))
-	check(*run(conf.ssh('router', f'ln -s /usr/libexec/vyos/conf_mode conf')))
-	check(*run(conf.ssh('router', f'ln -s /usr/libexec/vyos/op_mode op')))
+	check(*run(conf.ssh('router', f'ln -sf /usr/lib/python3/dist-packages/vyos vyos')))
+	check(*run(conf.ssh('router', f'ln -sf /usr/libexec/vyos/conf_mode conf')))
+	check(*run(conf.ssh('router', f'ln -sf /usr/libexec/vyos/op_mode op')))
 
 	for src, dst in move:
 		check(*run(conf.ssh('router', f'sudo chgrp -R vyattacfg {dst}')))
