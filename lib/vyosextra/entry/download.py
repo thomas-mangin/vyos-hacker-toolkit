0c765223defdaa2112f7f8ce95b48a08dedcb425
Thomas Mangin <thomas.mangin@exa.net.uk>
2020-04-28 23:06:33 +0100

    improve download to acces url and guess if a name is a vyos version

--- lib/vyosextra/entry/download.py
+++ lib/vyosextra/entry/download.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import re
 import sys
 import time
 import argparse
@@ -8,19 +9,70 @@ import datetime
 import urllib.request
 
 
-vyos_url = 'https://downloads.vyos.io/rolling/current/amd64/vyos-rolling-latest.iso'
-filename = 'vyos-rolling-latest.iso'
+vyos_rolling = '(vyos-1.3-rolling-[0-9]+-amd64.iso)'
 
-# modified from:
-# https://blog.shichao.io/2012/10/04/progress_speed_indicator_for_urlretrieve_in_python.html
 
-def download(url=vyos_url, filename=filename):
+def latest():
+	vyos_listing = "https://downloads.vyos.io/?dir=rolling/current/amd64"
+	regex = re.compile(f'data-name="{vyos_rolling}"')
+
+	try:
+		opener = urllib.request.FancyURLopener({})
+		with opener.open(vyos_listing) as f:
+			content = f.read().decode('utf-8')
+
+		found = regex.findall(content)
+		if not found:
+			return ''
+		found.sort()
+		return found[-1]
+	except Exception:
+		return ''
+
+
+def makeup(filename):
+	name = ''
+	location = ''
+
+	if filename:
+		base = filename.split('/')[-1]
+		if re.match(vyos_rolling, base):
+			if not filename.startswith('https://'):
+				location = filename
+			name = base
+
+	if not name:
+		try:
+			name = latest()
+		except KeyboardInterrupt:
+			sys.exit(2)
+		except Exception:
+			# should not happen, but something is better than nothing
+			name = 'vyos-rolling-latest.iso'
+
+	if not location:
+		location = name
+
+	url = f'https://downloads.vyos.io/rolling/current/amd64/{name}'
+
+	return (name, os.path.abspath(location), url)
+
+
+def download():
 	parser = argparse.ArgumentParser(description='download latest VyOS image')
-	parser.add_argument('-f', '--file', type=str, default=filename, help='iso file to save as')
+	parser.add_argument('-f', '--file', type=str, default='', help='iso file to save as')
 	args = parser.parse_args()
 
-	location = args.file
-	filename = os.path.basename(location)
+	name, location, url = makeup(args.file)
+	print(f'downloading {url}')
+	print(f'to          {location}')
+
+	if os.path.exists(location):
+		print(f'already downloaded iso file {name}')
+		sys.exit(1)
+
+	# modified from:
+	# https://blog.shichao.io/2012/10/04/progress_speed_indicator_for_urlretrieve_in_python.html
 
 	start_time = time.time()
 
@@ -31,7 +83,7 @@ def download(url=vyos_url, filename=filename):
 		speed = int(progress / (1024 * (int(duration) + 1)))
 		percent = min(int(count*block_size*100/total_size), 100)
 		progress_mb = progress / (1024 * 1024)
-		report = f'  {filename} {percent:>3}%, {progress_mb:3.2f} MB, {speed:>5} KB/s, {elapsed} seconds passed \r'
+		report = f'  {name} {percent:>3}%, {progress_mb:3.2f} MB, {speed:>5} KB/s, {elapsed} seconds passed \r'
 		sys.stdout.write(report)
 		sys.stdout.flush()
 
@@ -41,6 +93,11 @@ def download(url=vyos_url, filename=filename):
 	except KeyboardInterrupt:
 		print(f'\nremoving {location}')
 		os.remove(location)
+		sys.exit(2)
+	except Exception:
+		print(f'\nremoving {location}')
+		os.remove(location)
+		sys.exit(3)
 
 
 if __name__ == '__main__':
