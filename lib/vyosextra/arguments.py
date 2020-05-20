36eebe9dc669bc86f3427f5be400467e6901ee81
Thomas Mangin <thomas.mangin@exa.net.uk>
2020-05-20 13:33:59 +0100

    fix arg parsing for --sudo

--- lib/vyosextra/arguments.py
+++ lib/vyosextra/arguments.py
@@ -53,7 +53,7 @@ def _server(parser):
     default = config.default.get('build', None)
     nargs = '?' if default else None
     parser.add_argument('server', nargs=nargs, default=default, help='server on which the action will be performed')
-    parser.add_argument('--sudo', type='store_true', help='also setup sudo on this machine')
+    parser.add_argument('--sudo', action='store_true', help='also setup sudo on this machine')
 
 
 @register('package')
