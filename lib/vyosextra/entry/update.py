49f1287246bd3dfbd47312bb1bca8d1bec753dfa
Thomas Mangin <thomas.mangin@exa.net.uk>
2020-05-30 12:49:49 +0100

    progressive updating of file

--- lib/vyosextra/entry/update.py
+++ lib/vyosextra/entry/update.py
@@ -14,10 +14,11 @@ from vyosextra.config import config
 
 
 class Control(control.Control):
-    timed = []
     def permission(self, where, folder):
+        user = config.get(where, 'user')
         with Repository(folder, verbose=self.verbose):
             for src, dst in self.move:
+                self.ssh(where, f'sudo chown -R {user} {dst}')
                 self.ssh(where, f'sudo chgrp -R vyattacfg {dst}')
                 self.ssh(where, f'sudo chmod -R g+rxw {dst}')
 
@@ -26,7 +27,21 @@ class Control(control.Control):
             for src, dst in self.move:
                 self.run(config.rsync(where, src, dst))
 
-	
+    # there is no portable way, and python only to use inotify / etc.
+    def update(self, where, folder):
+        files = [os.path.join(dp, f) for dp, dn, fn in os.walk(os.path.expanduser(folder)) for f in fn]
+        modified = dict((f, os.path.getmtime(f)) for f in files if os.path.exists(f))
+        while True:
+            new_files = [os.path.join(dp, f) for dp, dn, fn in os.walk(os.path.expanduser(folder)) for f in fn]
+            new_modified = dict((f, os.path.getmtime(f)) for f in new_files if os.path.exists(f))
+
+            if new_modified != modified:
+                modified = new_modified
+                self.rsync(where, folder)
+
+            time.sleep(0.5)
+
+
 def main():
     'update a VyOS router filesystem with newer vyos-1x code'
     arg = arguments.setup(__doc__, ['update'])
@@ -40,9 +55,8 @@ def main():
         sys.exit(f'target "{arg.router}" is not a VyOS router\n')
 
     control.permission(arg.router, arg.working)
-    while True:
-        time.sleep(0.2)
-        control.rsync(arg.router, arg.working)
+    control.rsync(arg.router, arg.working)
+    control.update(arg.router, arg.working)
 
 
 if __name__ == '__main__':
